65dd6fd28a843f479cbd90ee2c980edae639811d
Jose Zamora <zamora_jose@stg-edu-136-168-86-208.dyn.csub.edu>
2023-01-25 09:32:10 -0800

    first commit

---
+++ lab1.cpp
@@ -0,0 +1,21 @@
+// Jose Zamora
+// Lab 1
+// 01 / 24 / 23
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    //cout << "Hello World!\n";
+   
+    // this are some changes that I have made
+
+    int a = rand() % 10 + 1;
+
+    cout << a << "\n";
+    cout << "Hello World!\n";
+    
+    return 0;
+}
